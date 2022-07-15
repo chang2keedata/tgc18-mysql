abcab936bd45445fc5eac5bddebc6e3c43343bbf
DK <jack.chang@trentglobal.edu.sg>
2022-07-15 06:14:10 +0000

    qs until 11

--- ClassicModelDatabaseQuestions.sql
+++ ClassicModelDatabaseQuestions.sql
@@ -9,15 +9,38 @@ SELECT * FROM orders where comments like '%fedex%';
 SELECT contactFirstName, contactLastName, customerName FROM customers order by customerName desc;
 
 -- 4 - Find all sales rep who are in office code 1, 2 or 3 and their first name or last name contains the substring 'son'
-SELECT * FROM employees where jobTitle = 'sales rep' and (firstName like '%son%' or lastName like '%son%');
+SELECT * FROM employees where officeCode = 1 or officeCode = 2 or officeCode = 3)
+and (firstName like '%son%' or lastName like '%son%') and jobTitle = 'sales rep' ;
 
 -- 5 - Display all the orders bought by the customer with the customer number 124, along with the customer name, the contact's first name and contact's last name.
 SELECT orderNumber, customerName, contactLastName, contactFirstName FROM customers join orders
     on customers.customerNumber = orders.customerNumber
     where customers.customerNumber = 124;
 
--- 6 - Show the name of the product, together with the order details,  for each order line from the orderdetails table
+-- 6 - Show the name of the product, together with the order details, for each order line from the orderdetails table
 SELECT orderLineNumber, products.productName FROM orderdetails
     left join orders on orderdetails.orderNumber = orders.orderNumber
     join products on orderdetails.productCode = products.productCode
     order by orderLineNumber asc;
+
+-- 7 - Display sum of all the payments made by each company from the USA. 
+SELECT customerName, sum(amount), country FROM customers join payments on customers.customerNumber = payments.customerNumber
+where country = 'usa' group by customerName, country;
+
+select payments.customerNumber, customerName, sum(amount) from payments
+join customers on payments.customerNumber = customers.customerNumber group by payments.customerNumber, customerName
+
+-- 8 - Show how many employees are there for each state in the USA
+SELECT count(employeeNumber), state FROM employees
+JOIN offices ON employees.officeCode = offices.officeCode WHERE country = 'usa' GROUP BY state
+
+-- 9 - From the payments table, display the average amount spent by each customer. Display the name of the customer as well.
+SELECT avg(amount), customerName FROM customers join payments on customers.customerNumber = payments.customerNumber
+group by payments.customerNumber, customerName;
+
+-- 10 - From the payments table, display the average amount spent by each customer but only if the customer has spent a minimum of 10,000 dollars.
+SELECT avg(amount), customerName FROM customers join payments on customers.customerNumber = payments.customerNumber
+group by payments.customerNumber, customerName having sum(amount) >= 10000;
+
+-- 11  - For each product, display how many times it was ordered, and display the results with the most orders first and only show the top ten.
+SELECT productCode, count(*) as times_ordered FROM orderdetails group by productCode order by count(*) desc limit 10;
--- data-query.sql
+++ data-query.sql
@@ -1,4 +1,4 @@
--- %sales% match as long as the word 'sales' appear anywhere in jobTitle
+-- %sales% match AS long AS the word 'sales' appear anywhere in jobTitle
 SELECT * FROM employees WHERE jobTitle LIKE '%sales%';
 SELECT * FROM employees WHERE jobTitle LIKE 'sales%';
 SELECT * FROM employees WHERE jobTitle LIKE '%sales';
@@ -8,11 +8,11 @@ SELECT * FROM employees WHERE jobTitle LIKE 'sales rep' AND (officeCode = 1 or o
 SELECT * FROM customers WHERE (country = 'USA' AND state = 'NV' AND creditLimit > 5000)
     OR creditLimit > 200000;
 
--- inner join
+-- inner JOIN (show all FROM both tables: employess.*, offices.*)
 SELECT * FROM employees e JOIN offices o ON e.officeCode = o.officeCode WHERE country = 'USA';
-SELECT firstName, lastName, customerName, salesRepEmployeeNumber, employeeNumber FROM customers c 
-    join employees e on c.salesRepEmployeeNumber = e.employeeNumber;
 
+SELECT firstName, lastName, customerName, salesRepEmployeeNumber, employeeNumber FROM customers c 
+    JOIN employees e ON c.salesRepEmployeeNumber = e.employeeNumber;
 
 SELECT customerName AS 'Customer Name', customers.country AS 'Customer Country', firstName, lastName, offices.phone
 FROM customers
@@ -24,14 +24,56 @@ WHERE customers.country = 'USA';
 
 -- date manipulation
 -- current date in server
-select curdate();
+SELECT curdate();
 -- the date and time
-select now();
+SELECT now();
 -- select after 30th June 2000
-select * from payments where payemntDate > '2000-06-30';
-select * from payments where payemntDate BETWEEn '2000-06-30' AND '2000-07-31';
--- show all payments made in the year 2000 
-select checkNumber, YEAR(payemntDate), MONTH(payemntDate), DAY(payemntDate) from payments
+select * from payments WHERE payemntDate > '2000-06-30';
+select * from payments WHERE payemntDate BETWEEN '2000-06-30' AND '2000-07-31';
+
+-- show all payments made in the year 2000
+SELECT checkNumber, YEAR(payemntDate), MONTH(payemntDate), DAY(payemntDate) FROM payments
     WHERE YEAR(paymentDate) = 2000;
 
+-- aggreagate functions
+-- count how many rows 
+SELECT count(*) FROM employees;
+
+-- sum, avg
+SELECT sum(quantityOrdered * priceEach) FROM orderdetails WHERE productCode = 'S18_1749';
+
+-- find the total amount paid BY customers in the month of June 2013
+SELECT sum(amount) FROM payments WHERE month(paymentDate) = 6 and year(paymentDate) = 2003;
+
+-- COUNT(*) count how many customers in each country GROUP
+SELECT country, count(*) AS total FROM customers GROUP BY country;
+SELECT country, avg(creditLimit), count(*) FROM customers GROUP BY country;
+
+-- HAVING
+SELECT country,
+         firstName,
+         lastName,
+         email,
+         avg(creditLimit),
+         count(*)
+FROM customers
+JOIN employees
+    ON customers.salesRepEmployeeNumber = employees.employeeNumber
+WHERE salesRepEmployeeNumber = 1504
+GROUP BY  country
+HAVING count(*) >= 3 
+ORDER BY  avg(creditLimit) DESC
+LIMIT 3
+
+-- intermediate level
+-- SUBQUERY
+SELECT * FROM customers WHERE crediLimit > (SELECT avg(creditLimit) FROM customers);
+
+SELECT * FROM products WHERE productCode NOT in (SELECT distinct(productCode) FROM orderdetails);
+
+SELECT firstname, lastName, employeeNumber, sum(amount) FROM employees
+JOIN customers ON employees.employeeNumber = customers.salesRepEmployeeNumber
+JOIN payments ON customers.customerNumber = payments.customerNumber
+GROUP BY employees.employeeNumber
+HAVING sum(amount) > (SELECT sum(amount) * 0.1 FROM payments);
 
